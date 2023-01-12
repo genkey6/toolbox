8b44e9cd1464ff383b227e314865e51b43a39a31
Shu Gen <gen_shu@caddi.jp>
2023-01-12 23:14:03 +0900

    add comment for avoid using IntrospectAndCompose in supergraph

--- typescript/apps/apollo-supergraph-gateway/src/main.ts
+++ typescript/apps/apollo-supergraph-gateway/src/main.ts
@@ -3,6 +3,8 @@ import { startStandaloneServer } from '@apollo/server/standalone';
 import { ApolloGateway, IntrospectAndCompose } from '@apollo/gateway';
 
 const gateway = new ApolloGateway({
+  // !! Avoid using `IntrospectAndCompose` in production, since it has some critical limitations.
+  // For details, see: https://www.apollographql.com/docs/apollo-server/using-federation/apollo-gateway-setup/#limitations-of-introspectandcompose
   supergraphSdl: new IntrospectAndCompose({
     subgraphs: [
       { name: 'reviews', url: 'http://localhost:4001/graphql' },
