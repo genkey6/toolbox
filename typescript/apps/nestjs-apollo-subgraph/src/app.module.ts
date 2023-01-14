08fead41049c90584b6663c673fb339e74cace56
Shu Gen <gen_shu@caddi.jp>
2023-01-14 15:07:30 +0900

    add auto generated schema file for nestjs-apollo-subgraph

--- typescript/apps/nestjs-apollo-subgraph/src/app.module.ts
+++ typescript/apps/nestjs-apollo-subgraph/src/app.module.ts
@@ -14,6 +14,7 @@ import { Zoo, ZooResolver } from './graphql/zoo.resolver';
       driver: ApolloFederationDriver,
       autoSchemaFile: {
         federation: 2,
+        path: 'apps/nestjs-apollo-subgraph/src/graphql/schema.graphqls',
       },
       buildSchemaOptions: {
         orphanedTypes: [Zoo],
